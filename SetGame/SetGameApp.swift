5798f4c35f6d74bf4da90a264fba708019e3870a
srgxzhrv <sergey_sentybr@mail.ru>
2025-10-06 19:49:16 +0200

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Сергей Захаров on 06.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
